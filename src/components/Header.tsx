a44ecea07028cd4a53ba99080b7eb519ccacd501
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-10-10 19:27:06 +0800

    dummy home page

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <nav className="header">
+      <Link to="/" className="header__link">
+        <img
+          className="header__logo"
+          src="https://www.dafont.com/forum/attach/orig/2/4/24836.png"
+          alt=""
+        />
+      </Link>
+      <Link to="/" className="header__link">
+        <div className="header__title">
+          <h1>Data Dofus</h1>
+        </div>
+      </Link>
+    </nav>
+  );
+}
