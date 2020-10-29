868c9470e94dcf9b5a004361930060c28252d1b5
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-10-29 23:21:31 +0800

    :sparkles: :poop: Dummy orderby

--- src/components/TableUsers.tsx
+++ src/components/TableUsers.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { db } from '../firebase';
 import * as interfaces from '../interfaces';
-import { Table } from 'react-bootstrap';
+import { Dropdown, DropdownButton, Table } from 'react-bootstrap';
 import UserRow from './UserRow';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,21 +9,64 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function TableUsers() {
 
     const [users, setUsers] = useState([] as interfaces.User[]);
+    const [orderBy, setOrderBy] = useState('level');
+    const [orderByDirection, setOrderByDirection] = useState('desc');
+
 
     useEffect(function() {
         onUseEffect();
     });
 
     async function onUseEffect() {
-        const querySnapshot = await db.collection('users').get();
+        const querySnapshot = await db.collection('users').orderBy(orderBy, orderByDirection as "asc"|"desc").limit(20).get();
         console.log('docs', querySnapshot.docs.length);
         setUsers(querySnapshot.docs.map(function(documentSnapshot) {
-            return documentSnapshot.data() as interfaces.User;
+            const user = documentSnapshot.data() as interfaces.User;
+            user.id = documentSnapshot.id;
+            return user;
         }));
     }
     
     return (
         <div className='table'>
+            <div className="table__filter">
+
+            </div>
+            <div className="table__orderBy">
+                <DropdownButton
+                    key='orderBy'
+                    id='dropdown-order-by'
+                    variant='secondary'
+                    title={`Order By: ${orderBy}`}
+                >
+                    {['id', 'level', 'Chance_base', 'Chance_bonus', 'Chance_total'].map(function(key) {
+                        return <Dropdown.Item
+                            eventKey={key}
+                            onSelect={function(eventKey, _){
+                                setOrderBy(eventKey as string);
+                            }}
+                        >
+                            {key}
+                        </Dropdown.Item>;
+                    })}
+                </DropdownButton>
+                <DropdownButton
+                    key='orderByDirection'
+                    id='dropdown-order-by-direction'
+                    variant='secondary'
+                    title={orderByDirection}
+                >
+                    {['desc', 'asc'].map(function(key) {
+                        return <Dropdown.Item
+                            eventKey={key}
+                            onSelect={(eventKey, _) => setOrderByDirection(eventKey as string)}
+                        >
+                            {key}
+                        </Dropdown.Item>;
+                    })}
+                </DropdownButton>
+
+            </div>
             <Table striped bordered hover size="sm" variant="dark">
                 <thead>
                     <tr>
